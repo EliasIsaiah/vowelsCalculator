1d45da10a863f23b660ed4de652504f123843af3
Elias Roussos <eliasisaiah@outlook.com>
2021-11-11 20:12:20 -0500

    noodling

--- vowel-counter/src/routes/index.svelte
+++ vowel-counter/src/routes/index.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flexContainer">
+    <h1>Vowel Counter</h1>
+    <div class="gray">
+        <h3>Enter your word here:</h3>
+        <input type="text" id="word" required />
+    </div>
+</div>
+
+<style>
+    .gray {
+        background-color: rgba(200, 200, 200, 0.3);
+        padding: 20px;
+        border-radius: 12px;
+    }
+    .flexContainer {
+        display: flex;
+        justify-content: center;
+    }
+</style>
